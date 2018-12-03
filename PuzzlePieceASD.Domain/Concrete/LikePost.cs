a53215bd8764ef666c4f068a696a2f2580e67249
Jacob Russell <jacob.russell82@outlook.com>
2018-12-03 15:09:44 -0500

    adds table for Liking a post

---
+++ PuzzlePieceASD.Domain/Concrete/LikePost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    public class LikePost
+    {
+        public int LikingUser { get; set; }
+
+        public int PostID { get; set; }
+    }
+}
