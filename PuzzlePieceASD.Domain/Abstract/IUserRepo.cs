96e9771981e6f845e729b0c1ddd140846b77c42b
Jacob Russell <jacob.russell82@outlook.com>
2018-12-03 14:48:08 -0500

    abstract interface to build Users

---
+++ PuzzlePieceASD.Domain/Abstract/IUserRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuzzlePieceASD.Domain.Abstract
+{
+    public interface IUserRepo
+    {
+        IEnumerable<IUserRepo> Users { get; }
+    }
+}
