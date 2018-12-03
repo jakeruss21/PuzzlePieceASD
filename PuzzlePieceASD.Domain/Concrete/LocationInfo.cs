7cc1fdad4798e5b98ebbb6f24c09ee71eb5cc0e9
Jacob Russell <jacob.russell82@outlook.com>
2018-12-03 15:01:09 -0500

    adds Local user and LocationInfo db

---
+++ PuzzlePieceASD.Domain/Concrete/LocationInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    public class LocationInfo
+    {
+        public int InfoID { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(25)]
+        public string State { get; set; }
+
+        [Range(5, 9)]
+        public Nullable<int> Zipcode { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ PuzzlePieceASD.Domain/Concrete/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    public class User
+    {
+        public int UserID { get; }
+
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Range(8, 50)]
+        [Required(ErrorMessage = "Password must be at least 8 characters long, and no longer than 50 characters")]
+        public string Password { get; set; }
+
+        [StringLength(20)]
+        public string UserType { get; set; }
+
+        public int UserBio { get; set; }
+
+    }
+}
+
