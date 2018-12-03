9d382d1ccd2717c7316ced90f395af6b1398fb57
Jacob Russell <jacob.russell82@outlook.com>
2018-12-03 15:11:07 -0500

    adds table for following user

---
+++ PuzzlePieceASD.Domain/Concrete/FollowUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    public class FollowUser
+    {
+        public int FollowingUser { get; set; }
+
+        public int UserToBeFollowed { get; set; }
+    }
+}
