2708500172b7ca53fb87805ae7a6894ba7cfa3bd
Jacob Russell <jacob.russell82@outlook.com>
2018-12-04 09:30:20 -0500

    had to move everything around in order to allow for .net Framework instead of .net Core

---
+++ PuzzlePieceASD.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PuzzlePieceASD.Domain.DAL;
+
+namespace PuzzlePieceASD.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: User Database
+        private UserContext db = new UserContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db);
+        }
+    }
+}
---
+++ PuzzlePieceASD.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PuzzlePieceASD.WebUI.MvcApplication" Language="C#" %>
