87b7496c0fd4aca95baf143bad4bb7232ae77f02
Jacob Russell <jacob.russell82@outlook.com>
2018-12-04 11:11:36 -0500

    adds UserTable with fk pointing to UserType table

---
+++ PuzzlePieceASD_Database/dbo.UserTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [UserBio] NVARCHAR(MAX) NULL, 
+    [UserType] INT NOT NULL
+)
