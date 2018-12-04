e9478b144ea92935c04a3ae309c2687f14a79dce
Jacob Russell <jacob.russell82@outlook.com>
2018-12-03 23:38:24 -0500

    adds seed data for database

---
+++ PuzzlePieceASD.Domain/DAL/UserSeedDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PuzzlePieceASD.Domain.Concrete;
+
+namespace PuzzlePieceASD.Domain.DAL
+{
+    public class UserSeedDatabase : System.Data.Entity.DropCreateDatabaseIfModelChanges<UserContext>
+    {
+        protected override void Seed (UserContext context)
+        {
+            var users = new List<User>
+            {
+                new User { UserName = "FlyingMonkey", FirstName = "Jake", LastName = "Russ", Password = "pass123", UserType = "Personal", UserBio = 1},
+                new User { UserName = "Schnitzleman", FirstName = "Philipp", LastName = "Schmidt", Password = "123pass", UserType = "Personal", UserBio = 2 },
+                new User { UserName = "NewBranchTherapy", FirstName = "New", LastName = "Branch", Password = "Pass!@#", UserType = "Service Provider", UserBio = 3 },
+                new User { UserName = "TrueYou", FirstName = "True", LastName = "You", Password = "!@#Pass", UserType = "Support Group", UserBio = 4 }
+            };
+        }
+    }
+}
