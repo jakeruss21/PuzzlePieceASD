3e4af6f6603de2f64c524459784fa1f82f26dc89
Jacob Russell <jacob.russell82@outlook.com>
2018-12-05 22:39:57 -0500

    add PhoneNumber class

---
+++ PuzzlePieceASD.Domain/Concrete/PhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    class PhoneNumber
+    {
+        public int PhoneNumberID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string PhoneNum { get; set; }
+    }
+}
