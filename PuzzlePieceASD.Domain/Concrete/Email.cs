3f74d65d6898b7d7366d811aaf020d8a3f374717
Jacob Russell <jacob.russell82@outlook.com>
2018-12-05 22:31:18 -0500

    add email address class (corrosponds to email table in database

---
+++ PuzzlePieceASD.Domain/Concrete/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    class Email
+    {
+        public int EmailID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
