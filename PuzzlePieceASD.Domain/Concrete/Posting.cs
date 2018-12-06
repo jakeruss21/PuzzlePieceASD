28afe2ded2a2fde6ad125efbbba86bc0a07826ad
Jacob Russell <jacob.russell82@outlook.com>
2018-12-05 22:41:45 -0500

    rename class to Posting to better allign with database

--- PuzzlePieceASD.Domain/Concrete/Posting.cs
+++ PuzzlePieceASD.Domain/Concrete/Posting.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace PuzzlePieceASD.Domain.Concrete
 {
-    public class Post
+    public class Posting
     {
         public int PostID { get; set; }
 
-        public int UserID_FK { get; set; }
+        public int UserID { get; set; }
 
-        public string Posting { get; set; }
+        public string Post { get; set; }
 
         public Nullable<System.DateTime> PostingTime { get; set; }
     }
