d48ec5c7419052e8d619ab479cc7a61fa0420d14
Jacob Russell <jacob.russell82@outlook.com>
2018-12-06 10:39:44 -0500

    adding login page with supporting ViewModel

---
+++ PuzzlePieceASD.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PuzzlePieceASD.Domain.DAL;
+using PuzzlePieceASD.WebUI.ViewModels;
+
+namespace PuzzlePieceASD.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (UserContext db = new UserContext())
+                {
+                    var user = db.Users.Where(a => a.UserName.Equals(model.UserName) &&
+                                a.Password.Equals(model.Password)).FirstOrDefault();
+
+                    if (user.UserName != null && user.Password != null)
+                    {
+                        return RedirectToAction("Please Login");
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ PuzzlePieceASD.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PuzzlePieceASD.Domain.DAL;
+
+namespace PuzzlePieceASD.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
