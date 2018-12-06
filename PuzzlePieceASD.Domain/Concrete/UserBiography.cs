20db966d80c38b4866c6bbe8edd217335b9296e6
Jacob Russell <jacob.russell82@outlook.com>
2018-12-05 22:43:24 -0500

    adds UserBiography class

---
+++ PuzzlePieceASD.Domain/Concrete/UserBiography.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlePieceASD.Domain.Concrete
+{
+    class UserBiography
+    {
+        public int BioID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string BioInfo { get; set; }
+    }
+}
